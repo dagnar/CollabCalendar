bcea4e0f54046bf69b10a23e993f4283afed8b0e
Thanh Truong <dagnar@gmail.com>
2013-05-27 23:56:46 -0700

    some login shits

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require 'pry'
 require "securerandom"
 require './models/user.rb'
 
+
 #for models
 Dir["./models/*.rb"].each { |file| require file }
 
@@ -35,7 +36,7 @@ end
 	  headers "Content-Type" => "text/html; charset=utf-8"
 	end
 
-	get '/' do
+	get '/user/new' do
 	  @title = "Hello"
 	  erb :form
 
@@ -55,11 +56,38 @@ end
 				:email => params[:post][:email],
 				:password => params[:post][:password]
 			)
-		  @name = "#{params[:post][:first_name]} #{params[:post][:last_name]}"
+
+			@name = "#{params[:post][:first_name]} #{params[:post][:last_name]}"
+			token = the_user.generate_token
+			#setting the cookie securely
+			# response.set_cookie(@name, {value: token, secure: true)
+			response.set_cookie(@name, token)
+		  
 		  
 		  @title = "Hello #{@name}"
 		  erb :hello
 		# end
+	end
+
+	get '/user/login' do
+		@title = "hello"
+		erb :login
+	end
+
+	post '/user/login' do
+		user = User.authenticate_password(params[:post][:email], params[:post][:password])
+		if user
+			'junk'
+			@name = "#{params[:post][:first_name]} #{params[:post][:last_name]}"
+			token = user.generate_token
+			response.set_cookie(@name, token)
+			content_type :json
+			return user.to_json(:except => [:_id, :role, :created_at, :password_hash, :token_hash])
+		else
+			return nil
+		end
+			# @title = "hello #{@name}"
+			# erb :hello
 	end
 
 	get '/users.json' do
--- models/user.rb
+++ models/user.rb
@@ -1,8 +1,11 @@
-require "digest"
+require 'digest'
 
 class User
 	include Mongoid::Document
 	include BCrypt
+
+	attr_accessor :password
+	before_save :encrypt_password
 
 	field :first_name, 						type: String
 	field :last_name, 						type: String
@@ -10,22 +13,22 @@ class User
 	field :role,									type: String, default: 'user'
 	field :password_hash, 				type: String
 	field :token_hash,						type: String
-	field :create_at, 						type: DateTime
+	field :created_at, 						type: DateTime
 
 	validates_presence_of :first_name, :last_name, :email
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "must be a valid email address"
-	validates_length_of :password, :minimum => 8, :maximum => 48, :message => "must be between 8 and 48 characters", :if => "password_changed?"
+	validates_length_of :password, :minimum => 8, :maximum => 48, :message => "must be between 8 and 48 characters", :if => "password_present?"
 	# validates_presense_of :password, :on => :create, :message => "Can't be blank."
 	validates_uniqueness_of :email, :message => "Already in use"
 	# validates :email, presence: true, length: { allow_blank: false}
 
-	attr_accessor :password
+
 
 	index({ email: 1 }, { unique: true, name: "email_index"})
 
 	def self.authenticate_password(email, password)
 		user = User.where(email: email).first
-		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+		if user && user.saved_password == password
 			user
 		else
 			nil
@@ -35,13 +38,16 @@ class User
 	def self.authenticate_token(email, token)
 		user = User.where(email: email).first
 		if user && user.token_hash = Digest::SHA1.hexdigest(token)
+			user
+		else
+			nil
+		end
 	end
 
-  def password_changed?
+  def password_present?
     if self.password.nil?
       false
     end
-    puts self.password
     true
   end
 
@@ -49,19 +55,24 @@ class User
     @saved_password ||= BCrypt::Password.new(password_hash)
   end
   
-  def saved_password=(new_password)
-    @saved_password = BCrypt::Password.create(new_password)
-    self.encrypted_password = @saved_password
-  end
+  # def saved_password=(new_password)
+  #   @saved_password = BCrypt::Password.create(new_password)
+  #   self.password_hash = @saved_password
+  # end
   
   def encrypt_password
-    saved_password = password if password.present?
+  	if password.present?
+  		@saved_password = BCrypt::Password.create(password)
+    	self.password_hash = @saved_password
+    	# saved_password = password if password.present?
+    end
   end
 
   def generate_token
   	begin
   		pre_hash_token = SecureRandom.hex
-  		self.update_attribute!(token_hash: Digest::SHA1.hexdigest(pre_hash_token))
+  		self.update_attribute(:token_hash, Digest::SHA1.hexdigest(pre_hash_token))
+  		self.save!
   		pre_hash_token
   	rescue 
   		return nil
